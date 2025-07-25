a95e644aa18ae1ad7f366003045907756672060a
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-07-25 20:32:17 +0300

    Create my_product.dart

---
+++ lib/models/my_product.dart
@@ -0,0 +1,211 @@
+import 'product.dart';
+
+class MyProducts {
+  static List<Product> allProducts = [
+    Product(
+      id: 1,
+      name: 'Nike Air Max 200',
+      price: 180.00,
+      image: 'assets/sneakers_pictures/1.png',
+      category: 'Trending Now',
+      description:
+      'Clean lines, versatile and timeless—the people shoe returns with the perfect look and feel.',
+      quantity: 1,
+    ),
+    Product(
+      id: 2,
+      name: 'Nike Originals',
+      price: 120.00,
+      image: 'assets/sneakers_pictures/2.png',
+      category: 'Out of Stock',
+      description:
+      'Nike Originals are, versatile and timeless—the people shoe returns with the perfect look and feel.',
+      quantity: 1,
+    ),
+    Product(
+      id: 3,
+      name: 'Adidas Ultraboost',
+      price: 160.00,
+      image: 'assets/sneakers_pictures/3.png',
+      category: 'Best Seller',
+      description:
+      'The Ultraboost delivers incredible energy return and comfort for all-day performance.',
+      quantity: 1,
+    ),
+    Product(
+      id: 4,
+      name: 'Puma RS-X',
+      price: 110.00,
+      image: 'assets/sneakers_pictures/4.png',
+      category: 'On Sale',
+      description:
+      'Bold and bulky, the RS-X mixes retro design with future-ready tech for standout looks.',
+      quantity: 1,
+    ),
+    Product(
+      id: 5,
+      name: 'New Balance 574',
+      price: 95.00,
+      image: 'assets/sneakers_pictures/5.png',
+      category: 'Classic',
+      description:
+      'Iconic style and cushioned support make the 574 a go-to for comfort and simplicity.',
+      quantity: 1,
+    ),
+    Product(
+      id: 6,
+      name: 'Leather Biker Jacket',
+      price: 220.00,
+      image: 'assets/jackets_pictures/1.png',
+      category: 'Jackets',
+      description:
+      'A premium leather biker jacket with a modern fit and timeless edge.',
+      quantity: 1,
+    ),
+    Product(
+      id: 7,
+      name: 'Denim Trucker Jacket',
+      price: 130.00,
+      image: 'assets/jackets_pictures/2.png',
+      category: 'Jackets',
+      description:
+      'Classic denim jacket with a rugged build and vintage wash for everyday wear.',
+      quantity: 1,
+    ),
+    Product(
+      id: 8,
+      name: 'Puffer Winter Jacket',
+      price: 180.00,
+      image: 'assets/jackets_pictures/3.png',
+      category: 'Jackets',
+      description:
+      'Stay warm with this insulated puffer jacket, perfect for cold weather.',
+      quantity: 1,
+    ),
+    Product(
+      id: 9,
+      name: 'Hooded Windbreaker',
+      price: 90.00,
+      image: 'assets/jackets_pictures/4.png',
+      category: 'Jackets',
+      description:
+      'Lightweight and water-resistant, great for active outdoor adventures.',
+      quantity: 1,
+    ),
+    Product(
+      id: 10,
+      name: 'Wool Blend Overcoat',
+      price: 250.00,
+      image: 'assets/jackets_pictures/5.png',
+      category: 'Jackets',
+      description:
+      'Elegant long coat made with a premium wool blend, ideal for formal or casual wear.',
+      quantity: 1,
+    ),
+  ];
+
+  static List<Product> sneakersList = [
+  Product(
+  id: 1,
+  name: 'Nike Air Max 200',
+  price: 180.00,
+  image: 'assets/sneakers_pictures/1.png',
+  category: 'Trending Now',
+  description:
+  'Clean lines, versatile and timeless—the people shoe returns with the perfect look and feel.',
+  quantity: 1,
+  ),
+  Product(
+  id: 2,
+  name: 'Nike Originals',
+  price: 120.00,
+  image: 'assets/sneakers_pictures/2.png',
+  category: 'Out of Stock',
+  description:
+  'Nike Originals are, versatile and timeless—the people shoe returns with the perfect look and feel.',
+  quantity: 1,
+  ),
+  Product(
+  id: 3,
+  name: 'Adidas Ultraboost',
+  price: 160.00,
+  image: 'assets/sneakers_pictures/3.png',
+  category: 'Best Seller',
+  description:
+  'The Ultraboost delivers incredible energy return and comfort for all-day performance.',
+  quantity: 1,
+  ),
+  Product(
+  id: 4,
+  name: 'Puma RS-X',
+  price: 110.00,
+  image: 'assets/sneakers_pictures/4.png',
+  category: 'On Sale',
+  description:
+  'Bold and bulky, the RS-X mixes retro design with future-ready tech for standout looks.',
+  quantity: 1,
+  ),
+  Product(
+  id: 5,
+  name: 'New Balance 574',
+  price: 95.00,
+  image: 'assets/sneakers_pictures/5.png',
+  category: 'Classic',
+  description:
+  'Iconic style and cushioned support make the 574 a go-to for comfort and simplicity.',
+  quantity: 1,
+  )];
+
+  static List<Product> jacketsList = [
+  Product(
+  id: 6,
+  name: 'Leather Biker Jacket',
+  price: 220.00,
+  image: 'assets/jackets_pictures/1.png',
+  category: 'Jackets',
+  description:
+  'A premium leather biker jacket with a modern fit and timeless edge.',
+  quantity: 1,
+  ),
+  Product(
+  id: 7,
+  name: 'Denim Trucker Jacket',
+  price: 130.00,
+  image: 'assets/jackets_pictures/2.png',
+  category: 'Jackets',
+  description:
+  'Classic denim jacket with a rugged build and vintage wash for everyday wear.',
+  quantity: 1,
+  ),
+  Product(
+  id: 8,
+  name: 'Puffer Winter Jacket',
+  price: 180.00,
+  image: 'assets/jackets_pictures/3.png',
+  category: 'Jackets',
+  description:
+  'Stay warm with this insulated puffer jacket, perfect for cold weather.',
+  quantity: 1,
+  ),
+  Product(
+  id: 9,
+  name: 'Hooded Windbreaker',
+  price: 90.00,
+  image: 'assets/jackets_pictures/4.png',
+  category: 'Jackets',
+  description:
+  'Lightweight and water-resistant, great for active outdoor adventures.',
+  quantity: 1,
+  ),
+  Product(
+  id: 10,
+  name: 'Wool Blend Overcoat',
+  price: 250.00,
+  image: 'assets/jackets_pictures/5.png',
+  category: 'Jackets',
+  description:
+  'Elegant long coat made with a premium wool blend, ideal for formal or casual wear.',
+  quantity: 1,
+  ),
+];
+}
